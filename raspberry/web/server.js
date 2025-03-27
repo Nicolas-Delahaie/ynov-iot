cb52fbd8662db029e5232763840d1a5104e71848
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2025-03-27 10:18:35 +0100

    web : 8080 => 80

--- raspberry/web/server.js
+++ raspberry/web/server.js
@@ -1,18 +1,18 @@
-const WebSocket = require('ws');
-const server = new WebSocket.Server({ host: '0.0.0.0', port: 8080 });
+const WebSocket = require("ws");
+const server = new WebSocket.Server({ host: "0.0.0.0", port: 80 });
 
-server.on('connection', socket => {
-    console.log('Un client est connecté');
+server.on("connection", (socket) => {
+  console.log("Un client est connecté");
 
-    socket.on('message', message => {
-        console.log(`Message reçu : ${message}`);
-        // Répondre au client
-        socket.send(`Serveur: ${message}`);
-    });
+  socket.on("message", (message) => {
+    console.log(`Message reçu : ${message}`);
+    // Répondre au client
+    socket.send(`Serveur: ${message}`);
+  });
 
-    socket.on('close', () => {
-        console.log('Un client s\'est déconnecté');
-    });
+  socket.on("close", () => {
+    console.log("Un client s'est déconnecté");
+  });
 });
 
-console.log('Serveur WebSocket démarré sur ws://localhost:8080');
+console.log("Serveur WebSocket démarré sur ws://localhost:80");
