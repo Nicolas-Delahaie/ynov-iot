e435a50ce45d4b1adc1972598c3f7724321c089a
SkyBlob12 <clement.andrieu12850@gmail.com>
2025-03-27 09:45:37 +0100

    Maj code esp32

--- raspberry/web/server.js
+++ raspberry/web/server.js
@@ -1,5 +1,5 @@
 const WebSocket = require('ws');
-const server = new WebSocket.Server({ port: 8080 });
+const server = new WebSocket.Server({ host: '0.0.0.0', port: 8080 });
 
 server.on('connection', socket => {
     console.log('Un client est connecté');
@@ -14,18 +14,5 @@ server.on('connection', socket => {
         console.log('Un client s\'est déconnecté');
     });
 });
-
-// Fonction pour envoyer des messages à tous les clients connectés
-function broadcastMessage() {
-    const message = `Message automatique : ok.`;
-    server.clients.forEach(client => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(message);
-        }
-    });
-}
-
-// Envoie un message toutes les 5 secondes
-setInterval(broadcastMessage, 5000);
 
 console.log('Serveur WebSocket démarré sur ws://localhost:8080');
